b7e2178394ac448f39749cd23c4bbb3d49fd8ddc
Judokasarin <Arunesh.sarin@gmail.com>
2023-03-13 00:54:45 +0530

    created some basic structure for dev-intro

--- src/app/components/dev-intro/dev-intro.component.ts
+++ src/app/components/dev-intro/dev-intro.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-dev-intro',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+  ],
   templateUrl: './dev-intro.component.html',
   styleUrls: ['./dev-intro.component.scss']
 })
